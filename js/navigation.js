7b61a30d3e31fefc3684525efbe5eaf46d8e8cda
inhll <ian.anders.hall@gmail.com>
2016-09-18 13:07:53 -0500

    Major revisions
    Updated the homepage layout. Modified images and navigational structure.

---
+++ js/navigation.js
@@ -0,0 +1,19 @@
+var work = [
+	["work-imgur.html", "Imgur Upload Enhancements", "imgur-splash.png"];
+	["https://medium.com/@inhll/enhancing-ux-for-the-cta-s-digital-signage-1ce484942fcd#.pqan3qbo2", "Chicago Transit Authority Signage Design", "cta-splash.png"];
+	["work-imgur.html", "Imgur Upload Enhancements", "imgur-splash.png"];
+	["work-imgur.html", "Imgur Upload Enhancements", "imgur-splash.png"];
+	["work-imgur.html", "Imgur Upload Enhancements", "imgur-splash.png"];
+	["work-imgur.html", "Imgur Upload Enhancements", "imgur-splash.png"];
+	];
+
+var function()
+
+        <div class="col-md-2">
+          <p>
+            <a href="work-summative.html" role="button">
+              <img src="img/summative-splash.png" class="img-responsive img-rounded">
+            </a>
+          </p>
+          <h5 class="add-proj">Desktop Usability Testing</h5>
+       </div>
